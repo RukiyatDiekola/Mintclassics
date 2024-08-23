d21382c7b78b63252b4a2fde132e9da2d2ade6e6
RukiyatDiekola <diekolarukiyat@gmail.com>
2024-08-23 20:36:25 +0100

    Updating queries

--- queries.sql
+++ queries.sql
@@ -1,2 +1,14 @@
+#Indicating the database to work on
 USE mintclassics;
+#to view the warehouses available
 SELECT * FROM warehouses;
+#To view the locations where customers order FROM
+SELECT DISTINCT city FROM customers;
+SHOW TABLES;
+SELECT * FROM products;
+#To explore products currently in inventory
+-- Retrieve details of products currently in inventory across all warehouses
+SELECT p.productCode, p.productName, p.productDescription, p.quantityInStock, w.warehouseName
+FROM products p
+JOIN warehouses w ON p.warehouseCode = w.warehouseCode
+WHERE p.quantityInStock > 0;
